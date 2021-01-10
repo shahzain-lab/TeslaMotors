daf91399a6dcfe1bae973f2df7d13308cb7d3d5c
shahzain <69709547+shahzain-lab@users.noreply.github.com>
2021-01-10 10:52:12 -0800

    create header layout

--- src/components/Header/Background.tsx
+++ src/components/Header/Background.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import './header.css';
 import { Header } from './Header';
+import TeslaLogo from '../../images/TeslaLogo.png'
 
 export const Background = () => {
     return (
+        <div className="big">
         <div className="background">
-            <div className="top_left"></div>
-            <Header />
+            <img className="teslaLogo" src={TeslaLogo} alt="tesla"/>
+            <div className="top_left">
+            </div>
             <div className="bottom_right"></div>
+        </div>
+            <Header />
         </div>
     )
 }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import darkcar from '../../images/darkcar.jpg'
-import './header.css'
+import './header.css';
+//img
 
 export const Header = () => {
     return (
         <div className="header">
-            
+         <h4>Make your journey with <br /><span className="tesla-head">Tesla</span></h4>
         </div>
     )
 }
