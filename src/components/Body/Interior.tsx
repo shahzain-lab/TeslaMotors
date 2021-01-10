61991756f9cf579766dbf2511428f798b96c4e1d
shahzain <69709547+shahzain-lab@users.noreply.github.com>
2021-01-10 14:37:13 -0800

    interior updated

--- src/components/Body/Interior.tsx
+++ src/components/Body/Interior.tsx
@@ -4,7 +4,8 @@ export const Interior = () => {
     return (
         <div className="Interior">
             <div className="InteriorClip">
-
+              <h3>Interior</h3>
+              <p>The interior of the Tesla Model 3 is now 100 percent leather-free. Recently,<br /> Tesla announced that the Model 3 is now totally free of leather, including the<br /> steering wheel. The company has also vowed that its forthcoming Model Y will<br /> also be fully vegan when it debuts in 2020.</p>
             </div>
         </div>
     )
